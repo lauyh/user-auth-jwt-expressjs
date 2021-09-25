677bfe555b3fdf3f342a590c403bd4e30e9cc3c5
Lau Yee Heng <lauyh87@gmail.com>
2021-09-26 00:39:53 +0800

    implement register route and function

--- authServer/routes/user.js
+++ authServer/routes/user.js
@@ -2,22 +2,58 @@
 
 const express = require('express');
 const router = express.Router();
-const {body} = require('express-validator')
-
+const {body, validationResult} = require('express-validator')
+const {v4:uuid} = require('uuid')
+const {controller} = require('../lib/controller')
+const {db} = require('../lib/sequelize');
+const {User} = require('../models/User')
+const {generate,compare} = require('../lib/pwdHelper')
 router.post(
     '/register', 
     body('username').isAlphanumeric(),
     body('email').isEmail().normalizeEmail(),
+    body('password').isLength({min: 6}),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()})
+        }
+        const t = db.transaction();
+        try {
+            const {username, email, password} = req.body;
+            let user = await User.findOne({where : {email:email}});
+            console.log(`user: ${user}`)
+            if(!user){
+                console.log('creating user')
+                const hash = generate(password);
+                const userid = uuid();
+                console.log('hash', hash);
+                console.log('uuid', userid);
+                const result = db.transaction(async (t) => {
+                    user = await User.create({id: userid,username:username, email: email, hash:hash}, {transaction: t});
+                    return user;
 
-    (req, res) => {
-
+                });
+                if(result){
+                    return res.status(201).json({error: '', message: 'Successfully register user.'});
+                }else{
+                    // await t.rollback();
+                    return res.status(400).json({error: '500', message: 'Fail to register user.'})
+                }
+            }else{
+                return res.status(400).json({error: '403', message: 'User already exist.'})
+            }
+        } catch (error) {
+            console.log(`error occurs when creating user: ${error}`)
+            // await t.rollback();
+        }
 })
 
 router.post(
     '/token',
     (req,res) => {
-
+       
     })
 
-    
+
 module.exports = router;
