d6bed7a8fefa05c1e7ce04610f9c5c2a0842f0f5
Lau Yee Heng <lauyh87@gmail.com>
2021-09-28 01:56:38 +0800

    Update User.js

--- authServer/src/models/User.js
+++ authServer/src/models/User.js
@@ -1,37 +1,37 @@
 'use strict'
 
-const {db} = require('../lib/sequelize');
-const {DataTypes} = require('sequelize');
+const {db} = require('../../lib/sequelize');
+const {Sequelize} = require('sequelize');
 
 
 const User = db.define('users',{
 	id:{
-		type: DataTypes.STRING,
+		type: Sequelize.STRING,
 		primaryKey: true,
 		allowNull: false,
 	},
 	username:{
-		type: DataTypes.STRING,
+		type: Sequelize.STRING,
 		allowNull: false
 	},
 	email:{
-		type: DataTypes.STRING,
+		type: Sequelize.STRING,
 		allowNull: false,
 		unique: true,
 		isEmail: true
 	},
 	hash: {
-		type: DataTypes.STRING,
+		type: Sequelize.STRING,
 		allowNull: false
 	},
 	status:{
-		type: DataTypes.STRING,
+		type: Sequelize.STRING,
 		allowNull: false,
 		isIn:[['active','banned']],
 		defaultValue: 'active',
 	},
 	roles:{
-		type: DataTypes.STRING,
+		type: Sequelize.STRING,
 		allowNull: false,
 		isIn: [['admin','client','customer support','seller']],
 		defaultValue: 'client'
