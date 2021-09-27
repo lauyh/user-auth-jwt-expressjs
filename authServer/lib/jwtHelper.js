b91076ca2b130551b38a07e90c2d57f18ebb0ba6
Lau Yee Heng <lauyh87@gmail.com>
2021-09-28 00:01:50 +0800

    fix the pem file issue(use priv not public)

--- authServer/lib/jwtHelper.js
+++ authServer/lib/jwtHelper.js
@@ -2,21 +2,21 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
 
-const pathToKey = path.join(__dirname, '../','cert/access_token_pub.pem')
+const pathToKey = path.join(__dirname, '../','cert/access_token_priv.pem')
 const PRIV_KEY = fs.readFileSync(pathToKey, 'utf8');
-const pathToRefreshKey = path.join(__dirname, '../','cert/refresh_token_pub.pem')
+const pathToRefreshKey = path.join(__dirname, '../','cert/refresh_token_priv.pem')
 const PRIV_KEY_REFRESH = fs.readFileSync(pathToRefreshKey, 'utf8');
 const ISSUER = 'ecomm-inhouse.net'
 
 const issueJWT = (user) => {
-    const id = user.id, expiredIn = '15m';
+    const id = user.id, expiredIn = '15s';
     const payload = {
         iss: ISSUER,
         sub: id,
         iat: Date.now(),
     };
 
-    return jwt.sign(payload, PRIV_KEY, {expiredIn: expiredIn, algorithm: 'RS256'});
+    return jwt.sign(payload, PRIV_KEY, {expiresIn: expiredIn, algorithm: 'RS256'});
 }
 
 
@@ -28,7 +28,7 @@ const issueRefreshToken = (user) => {
         iat: Date.now(),
     };
 
-    return jwt.sign(payload, PRIV_KEY_REFRESH, {expiredIn: expiredIn, algorithm: 'RS256'});
+    return jwt.sign(payload, PRIV_KEY_REFRESH, {expiresIn: expiredIn, algorithm: 'RS256'});
 }
 
 module.exports = {issueJWT,issueRefreshToken};
