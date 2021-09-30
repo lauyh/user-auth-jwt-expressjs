caccd2291f3c78236298c1d8a0f2b965e4129b4b
Lau Yee Heng <lauyh87@gmail.com>
2021-10-01 00:59:48 +0800

    add mongoose connection

--- authServer/src/app.js
+++ authServer/src/app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan')
 const app = express();
+const mongoose = require('mongoose');
+
+
 require('dotenv').config({path: require('find-config')('.env')})
 
 const PORT = process.env.PORT || 9000;
@@ -18,5 +21,9 @@ app.use(morgan()); // logger
 app.use('/api', require('./routes/user'))
 
 app.listen(PORT, () => {
+    mongoose.connect('mongodb://localhost:27017/refresh_token')
+        .then((result) =>{console.log(`Connected to mongodb`)})
+        .catch((err)=>{console.log(`Error in connecting to mongodb. ${err}`)})
+
     console.log(`listening on port ${PORT}`);
 })
